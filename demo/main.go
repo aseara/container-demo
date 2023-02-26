3c64cc027763b605719d2613e914d474f31146cc
aseara <aseara@163.com>
2023-02-26 21:57:05 +0800

    change hostname in container

--- demo/main.go
+++ demo/main.go
@@ -11,6 +11,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("bad command")
 	}
@@ -18,6 +20,25 @@ func main() {
 
 func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+
+	err := cmd.Run()
+	must(err)
+}
+
+func child() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	err := syscall.Sethostname([]byte("container"))
+	must(err)
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
@@ -28,7 +49,7 @@ func run() {
 		Cloneflags: syscall.CLONE_NEWUTS,
 	}
 
-	err := cmd.Run()
+	err = cmd.Run()
 	must(err)
 }
 
